d55518455b90e70ac5837e45eee6d5b76159bbab
Roy Stobbelaar <roystobbelaar@hotmail.com>
2017-07-10 21:43:36 +0200

    some ui changes. added areas

--- src/app/explore/explore.component.ts
+++ src/app/explore/explore.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ExploreArea } from 'app/logic/exploreArea';
 
 @Component({
     selector: 'explore',
@@ -6,6 +7,36 @@ import { Component } from '@angular/core';
     templateUrl: './explore.component.html'
 })
 
-export class ExploreComponent {
+export class ExploreComponent implements OnInit {
+
     public title: string = 'Explore component here!';
+    public exploreAreas: ExploreArea[];
+
+    ngOnInit(): void {
+        //Fill list with explore areas
+        this.exploreAreas = new Array();
+        this.exploreAreas.push(
+            {
+                imagePath: './assets/area/forest.png',
+                areaName: 'Forest of Origin',
+                areaLevel: 1,
+                areaLevelRequirement: 1,
+                areaClassRequirement: 'None',
+                maxAmountOfHeroes: 4
+            }, {
+                imagePath: './assets/area/cave.png',
+                areaName: 'Cave of Ttrials',
+                areaLevel: 2,
+                areaLevelRequirement: 1,
+                areaClassRequirement: 'None',
+                maxAmountOfHeroes: 4
+            }, {
+                imagePath: './assets/area/road.png',
+                areaName: 'Road to Seriel',
+                areaLevel: 4,
+                areaLevelRequirement: 2,
+                areaClassRequirement: 'None',
+                maxAmountOfHeroes: 4
+            })
+    }
 }
---
+++ src/app/logic/explorearea.ts
@@ -0,0 +1,13 @@
+
+//Class for explore area's.
+//Can have lvl/class requirements, maybe image of max amount of hero's oid.
+export class ExploreArea {
+
+    public imagePath: string;
+    public areaName: string;
+    public areaLevel: number;
+    public areaLevelRequirement?: number;
+    public areaClassRequirement?: string;
+    public maxAmountOfHeroes: number;
+
+}
